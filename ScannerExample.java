3860bcb30c860cea51d39ea278003baf75045b70
gxnny19 <wkdrudrjs010@gmail.com>
2025-09-01 16:56:36 +0900

    0901

---
+++ ScannerExample.java
@@ -0,0 +1,35 @@
+package ch02.sec01;
+
+import java.util.Scanner;
+
+public class ScannerExample {
+
+	public static void main(String[] args) throws Exception {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("x 값 입력");
+		String srtX = scanner.nextLine();
+		int x = Integer.parseInt(srtX);
+		
+		System.out.println("y 값 입력");
+		String srtY = scanner.nextLine();
+		int y = Integer.parseInt(srtY);
+		
+		int result = x + y;
+		System.out.println("x + y: " + result);
+		System.out.println();
+		
+		while(true) {
+			System.out.println("입력 문자열: ");
+			String data = scanner.nextLine();
+			if(data.equals("q")) {
+				break;
+			}
+			System.out.println("출력 문자열: " + data);
+			System.out.println();
+		}
+		
+		System.out.println("종료");
+	}
+
+}
---
+++ variableInitializationExample.java
@@ -0,0 +1,14 @@
+package ch02.sec01;
+
+public class variableInitializationExample {
+
+	public static void main(String[] args) {
+		
+		int value = 9;
+		
+		int result = value + 10;
+		
+		System.out.println(result);
+	}
+
+}
