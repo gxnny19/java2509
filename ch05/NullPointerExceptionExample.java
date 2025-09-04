63de925b40542d1e3073cc1068e272abe81bacb6
gxnny19 <wkdrudrjs010@gmail.com>
2025-09-04 17:22:44 +0900

    0904

---
+++ ch05/NullPointerExceptionExample.java
@@ -0,0 +1,12 @@
+package ch05;
+
+public class NullPointerExceptionExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] intArray = null;
+		
+		String str = null;
+	}
+
+}
